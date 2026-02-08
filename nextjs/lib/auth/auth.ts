b066a67976e249ea702916a3eed3d031b13d327d
Elias ZANOTTI <zanotti.elias@gmail.com>
2026-02-08 10:22:21 +0100

    feat: finish better auth setup for email and pass

--- nextjs/lib/auth/auth.ts
+++ nextjs/lib/auth/auth.ts
@@ -6,4 +6,10 @@ export const auth = betterAuth({
 	database: prismaAdapter(prisma, {
 		provider: "postgresql",
 	}),
+	emailAndPassword: {
+		enabled: true,
+		autoSignIn: true,
+		minPasswordLength: 8,
+		maxPasswordLength: 20,
+	},
 });
